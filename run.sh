60c877740e990b1b575caf6bf355a22fcbdc3ed4
Xinyao Niu <morryniu123@gmail.com>
2020-06-01 01:22:52 +1000

    update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 file=naive_player
 
-python runner.py -r $file -b StaffTeamEasy.myPlayer \
-    --delay 0 --blueName=$file --redName=myPlayer -m 1 --setRandomSeed=50 -q
-
-
 #python runner.py -r $file -b StaffTeamEasy.myPlayer \
-#    --delay 0 --redName=$file --blueName=myPlayer -m 500 -q
+#    --delay 0 --blueName=$file --redName=myPlayer -m 1 --setRandomSeed=50 -q
+
+
+python runner.py -r $file -b StaffTeamEasy.myPlayer \
+    --delay 0 --redName=$file --blueName=myPlayer -m 500 -q
